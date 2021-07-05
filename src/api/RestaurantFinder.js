3493dd4106d9b508412f055f426f3f5828e8bf51
abhii6700 <abhishekyadav6700.ay@gmail.com>
2021-07-05 16:39:15 +0530

    added headers in api

--- src/api/RestaurantFinder.js
+++ src/api/RestaurantFinder.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
+    headers: { 'Access-Control-Allow-Origin': '*' },
     baseURL: "https://restaurant-finder-abhii6700.herokuapp.com//api/v1/restaurants"
 })
