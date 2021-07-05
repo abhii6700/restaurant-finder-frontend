ada3a928398db45f7b1c728e9df68a425e0abc3c
abhii6700 <abhishekyadav6700.ay@gmail.com>
2021-07-05 16:47:14 +0530

    updated base url

--- src/api/RestaurantFinder.js
+++ src/api/RestaurantFinder.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     headers: { 'Access-Control-Allow-Origin': '*' },
-    baseURL: "https://restaurant-finder-abhii6700.herokuapp.com//api/v1/restaurants"
+    baseURL: "https://restaurant-finder-abhii6700.herokuapp.com/api/v1/restaurants"
 })
